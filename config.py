2f69b97f0bb92b22f76ea441c7d2b2e04bcf2b8d
amirabotta-beep <amira.botta@gmail.com>
2026-07-01 01:18:48 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# =========================================================
+# ⚙️  ملف الإعدادات — لا ترفعه على GitHub
+# =========================================================
+GITHUB_TOKEN   = "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"
+TELEGRAM_TOKEN = "8721590192:AAFcAdOZaiDyL5P1D24r6qDPvvwTJ4_i8ek"
+TELEGRAM_CHAT  = "8080767364"
+REPO_NAME      = "V00ltTech/app"
+APP_FILE_PATH  = "my_app.apk"
