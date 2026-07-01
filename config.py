7da853d48de40bbbf5acd7146536b99c41473534
amirabotta-beep <amira.botta@gmail.com>
2026-07-01 10:14:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # =========================================================
 # ⚙️  ملف الإعدادات — لا ترفعه على GitHub
 # =========================================================
-GITHUB_TOKEN   = "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"
+GITHUB_TOKEN   = "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"
 TELEGRAM_TOKEN = "8721590192:AAFcAdOZaiDyL5P1D24r6qDPvvwTJ4_i8ek"
 TELEGRAM_CHAT  = "8080767364"
 REPO_NAME      = "V00ltTech/app"
